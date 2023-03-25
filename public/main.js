296307eb66dc80fdc08639e58d0a542d978eef53
TimiMaliki <malikitimi@gmail.com>
2023-03-25 12:04:52 -0700

    first commit

---
+++ public/main.js
@@ -0,0 +1,39 @@
+
+ const inputForm = document.querySelector('#input');
+  const btn = document.querySelector('#button');
+
+
+
+
+
+ let newDate = new Date(1999,5,11);
+    birthDay = newDate.getFullYear();
+
+    let  presentYear = new Number(2023);
+
+     let age = presentYear - birthDay;
+
+   
+
+//    inputForm = calcAge => {
+
+//     let newDate = new Date(1999,5,11);
+//    birthDay = newDate.getFullYear();
+
+//    let  presentYear = new Number(2023);
+
+//     let age = presentYear - birthDay;
+//    }
+
+    button.addEventListener('click' , () =>{
+        console.log(age);
+             })
+
+  
+
+
+
+
+
+
+
